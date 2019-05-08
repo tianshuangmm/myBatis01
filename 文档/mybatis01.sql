4ed8313b9c8bd933543a454579e6db880df1719a
ts <15313871502@163.com>
2019-05-08 10:25:43 +0800

    提交

--- 文档/mybatis01.sql
+++ 文档/mybatis01.sql
@@ -1,4 +1,5 @@
 /*
+https://blog.csdn.net/fulq1234/article/details/55261559
 Navicat MySQL Data Transfer
 
 Source Server         : localhost_3306
@@ -10,10 +11,44 @@ Target Server Type    : MYSQL
 Target Server Version : 50714
 File Encoding         : 65001
 
-Date: 2019-05-05 17:47:11
+Date: 2019-05-08 10:25:03
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for command
+-- ----------------------------
+DROP TABLE IF EXISTS `command`;
+CREATE TABLE `command` (
+  `id` int(11) NOT NULL,
+  `name` varchar(16) NOT NULL,
+  `description` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of command
+-- ----------------------------
+INSERT INTO `command` VALUES ('1', 'name1', 'desc1');
+
+-- ----------------------------
+-- Table structure for command_content
+-- ----------------------------
+DROP TABLE IF EXISTS `command_content`;
+CREATE TABLE `command_content` (
+  `id` int(11) NOT NULL,
+  `command_id` int(11) DEFAULT NULL,
+  `content` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of command_content
+-- ----------------------------
+INSERT INTO `command_content` VALUES ('1', '1', 'content1');
+INSERT INTO `command_content` VALUES ('2', '1', 'content2');
+INSERT INTO `command_content` VALUES ('3', '1', 'content3');
 
 -- ----------------------------
 -- Table structure for message
@@ -21,13 +56,14 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `message`;
 CREATE TABLE `message` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `COMMAND` varchar(16) DEFAULT NULL,
-  `DESCRIPTION` varchar(32) DEFAULT NULL,
+  `COMMAND` varchar(60) DEFAULT NULL,
+  `DESCRIPTION` varchar(60) DEFAULT NULL,
   `CONTENT` varchar(2048) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of message
 -- ----------------------------
-INSERT INTO `message` VALUES ('1', '2', '2', '2');
+INSERT INTO `message` VALUES ('9', '我是第9个指令', '9', 'hello，word！9');
+INSERT INTO `message` VALUES ('11', '使用useGeneratedKey添加', '添加一条数据', 'id设置为自增');
